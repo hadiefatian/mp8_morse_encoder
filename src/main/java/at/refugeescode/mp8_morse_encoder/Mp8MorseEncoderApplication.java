0398647605d50e78eef8e0df96ea78a5024e0ba6
Hadi <hadi.effatian@gmail.com>
2018-04-12 16:43:25 +0200

    Created project

---
+++ src/main/java/at/refugeescode/mp8_morse_encoder/Mp8MorseEncoderApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp8_morse_encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp8MorseEncoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp8MorseEncoderApplication.class, args);
+	}
+}
